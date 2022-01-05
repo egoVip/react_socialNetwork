43e8da1e6770d5d186f5605066dcc740e9ba55fd
Stepan <egovip@mail.ri>
2022-01-05 20:57:41 +0700

    added action creator

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,41 +1,42 @@
 import React from "react";
 import style from './MyPosts.module.css';
 import Post from "./Post/Post";
-
+import { updateNewPostTextActionCreator } from "../../../redux/state";
+import { addPostActionCreator } from "../../../redux/state";
 
 const MyPosts = (props) => {
 
-  let postsElements = props.posts.map(phrase => <Post messageLike={phrase.messageLike} likesCount={phrase.likesCount} />);
- 
-  let newPostelement = React.createRef();
+	let postsElements = props.posts.map(phrase => <Post messageLike={phrase.messageLike} likesCount={phrase.likesCount} />);
 
-  let addPost = () => {
-    props.addPost();
-    props.updateNewPostText("");
-  };
+	let newPostelement = React.createRef();
 
-  let onPostChange = ()=>{
-    let text = newPostelement.current.value;
-    props.updateNewPostText(text);
-  };
-  return (
-    <div className={style.content}>
-      <div className={style.postsBlock}>
-        <h3>My posts</h3>
-        <div>
-          <div>
-            <textarea onChange={onPostChange} ref={newPostelement} value = {props.newPostText}/>
-          </div>
-          <div>
-            <button onClick={addPost}>Add post</button>
-          </div>
-        </div>
-        <div className={style.posts}>
-          {postsElements}
-        </div>
-      </div>
-    </div>
-  )
+	let addPost = () => {
+		props.dispatch(addPostActionCreator());
+		// props.updateNewPostText("");
+	};
+
+	let onPostChange = () => {
+		let text = newPostelement.current.value;
+		props.dispatch(updateNewPostTextActionCreator(text));
+	};
+	return (
+		<div className={style.content}>
+			<div className={style.postsBlock}>
+				<h3>My posts</h3>
+				<div>
+					<div>
+						<textarea onChange={onPostChange} ref={newPostelement} value={props.newPostText} />
+					</div>
+					<div>
+						<button onClick={addPost}>Add post</button>
+					</div>
+				</div>
+				<div className={style.posts}>
+					{postsElements}
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default MyPosts; 
--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import App from './App';
 let renderEntireTree = (state) =>{
   ReactDOM.render(
     <React.StrictMode>
-      <App state={state} addPost={store.addPost.bind(store)} updateNewPostText={store.updateNewPostText}/>
+      <App state={state} dispatch={store.dispatch.bind(store)} />
     </React.StrictMode>,
     document.getElementById('root')
   );
