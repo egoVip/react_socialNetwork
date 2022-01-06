48b7750e89c9ab6ef19f2ba8f9a9119e2d18518c
Stepan <egovip@mail.ri>
2022-01-06 18:42:48 +0700

    added textarea send message

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,19 +2,25 @@ import React from 'react';
 import style from './Dialogs.module.css'
 import DialogItem from './DialogItem/DialogItem';
 import Message from './Message/Message';
-
-let newMessage = React.createRef();
-
-let addMessage = ()=>{
-    let text = newMessage.current.value;
-    alert(text)
-}
+import { updateNewMessageBodyCreator } from '../../redux/state';
+import { sendMessageCreator } from '../../redux/state';
 
 const Dialogs = (props) => {
 
-    let dialogElements = props.state.dialogs.map(dialog => <DialogItem name={dialog.name} id={dialog.id} />);
+    let state = props.store.getState().dialogsPage;
 
-    let messagesElements = props.state.messages.map(phrase => <Message message={phrase.message} id={phrase.id} />);
+    let dialogElements = state.dialogs.map(dialog => <DialogItem name={dialog.name} id={dialog.id} />);
+    let messagesElements = state.messages.map(phrase => <Message message={phrase.message} id={phrase.id} />);
+    let newMessageBody = state.newMessageBody;
+
+    let onSendMessageClick = () => {
+        props.store.dispatch(sendMessageCreator())
+    }
+
+    let onNewMessageChange = (event) => {
+        let body = event.target.value;
+        props.store.dispatch(updateNewMessageBodyCreator(body))
+    }
 
     return (
         <div className={style.dialogs}>
@@ -22,10 +28,12 @@ const Dialogs = (props) => {
                 {dialogElements}
             </div>
             <div className={style.messages}>
-                {messagesElements}
+                <div>{messagesElements}</div>
+                <div>
+                    <div> <textarea value={newMessageBody} onChange={onNewMessageChange}></textarea></div><br />
+                    <div><button onClick={onSendMessageClick}>Send message</button></div>
+                </div>
             </div>
-            <textarea ref={newMessage}></textarea><br />
-            <button onClick={addMessage}>Send message</button>
         </div>
     )
 }
--- src/redux/state.js
+++ src/redux/state.js
@@ -48,7 +48,7 @@ let store = {
         this._callSubscriber = observer;
     },
 
-    
+
     dispatch(action) {
         if (action.type === ADD_POST) {
             let newPost = {
@@ -62,8 +62,13 @@ let store = {
         } else if (action.type === UPDATE_NEW_POST_TEXT) {
             this._state.profilePage.newPostText = action.newText;
             this._callSubscriber(this._state);
-        } else if(action.type === UPDATE_NEW_MESSAGE_BODY){
+        } else if (action.type === UPDATE_NEW_MESSAGE_BODY) {
             this._state.dialogsPage.newMessageBody = action.body;
+            this._callSubscriber(this._state);
+        } else if (action.type === SEND_MESSAGE) {
+            let body = this._state.dialogsPage.newMessageBody
+            this._state.dialogsPage.newMessageBody = '';
+            this._state.dialogsPage.messages.push({ id: 6, message: body });
             this._callSubscriber(this._state);
         }
 
@@ -71,11 +76,13 @@ let store = {
 
 };
 
-export const addPostActionCreator = () => ({type: ADD_POST});       //сокращенный синтаксиси return {type: ADD_POST}
+export const addPostActionCreator = () => ({ type: ADD_POST });       //сокращенный синтаксиси return {type: ADD_POST}
 
-export const updateNewPostTextActionCreator = (text) => ({type: UPDATE_NEW_POST_TEXT, newText: text});
+export const updateNewPostTextActionCreator = (text) => ({ type: UPDATE_NEW_POST_TEXT, newText: text });
 
+export const updateNewMessageBodyCreator = (body) => ({ type: UPDATE_NEW_MESSAGE_BODY, body: body });
 
+export const sendMessageCreator = () => ({ type: SEND_MESSAGE });
 
 window.store = store;
 export default store;
