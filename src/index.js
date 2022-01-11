4bf8d85518e2f305f684e2c35aa9741ccf5b0aae
Stepan <egovip@mail.ri>
2022-01-11 20:41:47 +0700

    copy state in ProfileReducer

--- src/index.js
+++ src/index.js
@@ -7,23 +7,21 @@ import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
 
-let renderEntireTree = () =>{
-  ReactDOM.render(
-    <React.StrictMode>
-      <Provider store={store}>
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
       <App />
-      </Provider>
-    </React.StrictMode>,
-    document.getElementById('root')
-  );
-}
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
-renderEntireTree();
 
-store.subscribe(()=>{
-  let state = store.getState()
-  renderEntireTree(state);
-});
+// store.subscribe(()=>{
+// let state = store.getState()
+//   renderEntireTree();
+// });
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
