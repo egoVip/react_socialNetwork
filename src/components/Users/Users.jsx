686fa8ff6d6e4b7697e73a51328622fa62727187
Stepan <egovip@mail.ri>
2022-01-15 19:38:34 +0700

    Users component added Follow/Unfollow

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,16 +1,39 @@
 import React from "react";
-
+import style from './Users.module.css'
 
 const Users = (props) => {
+    // if (props.users.length ===0){
+    // props.setUsers(
+    //     users:[
+    //         {
+    //             id: 1, photoUrl: 'https://resizer.mail.ru/p/7a1e86df-9686-5098-963e-27304c6f1133/AAACY37OntEVxnf9OOUuLkcQpyEwVtb3AZgK14eU9OI6IljiYtBizDmEp-vG8UfPC3h-OB130PE-ba1mk1rY6S-3Zek.jpg',
+    //             followed: true, fullName: 'Dima', status: 'Boss', location: { city: 'Minsk', country: 'Belarus' }
+    //         },
+    //         {
+    //             id: 2, photoUrl: 'https://resizer.mail.ru/p/7a1e86df-9686-5098-963e-27304c6f1133/AAACY37OntEVxnf9OOUuLkcQpyEwVtb3AZgK14eU9OI6IljiYtBizDmEp-vG8UfPC3h-OB130PE-ba1mk1rY6S-3Zek.jpg',
+    //             followed: true, fullName: 'Lena', status: 'Boss', location: { city: 'Moscow', country: 'Russia' }
+    //         },
+    //         {
+    //             id: 3, photoUrl: 'https://resizer.mail.ru/p/7a1e86df-9686-5098-963e-27304c6f1133/AAACY37OntEVxnf9OOUuLkcQpyEwVtb3AZgK14eU9OI6IljiYtBizDmEp-vG8UfPC3h-OB130PE-ba1mk1rY6S-3Zek.jpg',
+    //             followed: false, fullName: 'Stepa', status: 'Boss', location: { city: 'Kiev', country: 'Ukrain' }
+    //         },
+    //         {
+    //             id: 4, photoUrl: 'https://resizer.mail.ru/p/7a1e86df-9686-5098-963e-27304c6f1133/AAACY37OntEVxnf9OOUuLkcQpyEwVtb3AZgK14eU9OI6IljiYtBizDmEp-vG8UfPC3h-OB130PE-ba1mk1rY6S-3Zek.jpg',
+    //             followed: false, fullName: 'Vova', status: 'Boss', location: { city: 'Berlin', country: 'Germany' }
+    //         },
+    //     ]
+    // )}
     return <div>
         {
             props.users.map(u => <div key={u.id}>
                 <span>
                     <div>
-                        <img src={u.photoUrl} alt="" />
+                        <img src={u.photoUrl} className={style.userPhoto} />
                     </div>
                     <div>
-                        <button>Follow</button>
+                        {u.followed
+                            ? <button onClick={() => { props.toggleFollow(u.id) }}>Follow</button>
+                            : <button onClick={() => { props.toggleFollow(u.id) }}>Unfollow</button>}
                     </div>
                 </span>
                 <span>
@@ -24,11 +47,11 @@ const Users = (props) => {
                     </span>
                 </span>
             </div>
-                )
-            })
+            )
         }
+
     </div>
-    
+
 }
 
 export default Users;
--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -3,14 +3,22 @@ const SET_USERS = 'SET_USERS;'
 
 let initialState = {
     users: [
-        { id: 1, photoUrl: 'https://resizer.mail.ru/p/7a1e86df-9686-5098-963e-27304c6f1133/AAACY37OntEVxnf9OOUuLkcQpyEwVtb3AZgK14eU9OI6IljiYtBizDmEp-vG8UfPC3h-OB130PE-ba1mk1rY6S-3Zek.jpg',
-         followed: true, fullName: 'Dima', status: 'Boss', location: { city: 'Minsk', country: 'Belarus' } },
-        { id: 2, photoUrl: 'https://resizer.mail.ru/p/7a1e86df-9686-5098-963e-27304c6f1133/AAACY37OntEVxnf9OOUuLkcQpyEwVtb3AZgK14eU9OI6IljiYtBizDmEp-vG8UfPC3h-OB130PE-ba1mk1rY6S-3Zek.jpg',
-         followed: true, fullName: 'Lena', status: 'Boss', location: { city: 'Moscow', country: 'Russia' } },
-        { id: 3, photoUrl: 'https://resizer.mail.ru/p/7a1e86df-9686-5098-963e-27304c6f1133/AAACY37OntEVxnf9OOUuLkcQpyEwVtb3AZgK14eU9OI6IljiYtBizDmEp-vG8UfPC3h-OB130PE-ba1mk1rY6S-3Zek.jpg',
-         followed: false, fullName: 'Stepa', status: 'Boss', location: { city: 'Kiev', country: 'Ukrain' } },
-        { id: 4, photoUrl: 'https://resizer.mail.ru/p/7a1e86df-9686-5098-963e-27304c6f1133/AAACY37OntEVxnf9OOUuLkcQpyEwVtb3AZgK14eU9OI6IljiYtBizDmEp-vG8UfPC3h-OB130PE-ba1mk1rY6S-3Zek.jpg',
-         followed: false, fullName: 'Vova', status: 'Boss', location: { city: 'Berlin', country: 'Germany' } },
+        {
+            id: 1, photoUrl: 'https://resizer.mail.ru/p/7a1e86df-9686-5098-963e-27304c6f1133/AAACY37OntEVxnf9OOUuLkcQpyEwVtb3AZgK14eU9OI6IljiYtBizDmEp-vG8UfPC3h-OB130PE-ba1mk1rY6S-3Zek.jpg',
+            followed: true, fullName: 'Dima', status: 'Boss', location: { city: 'Minsk', country: 'Belarus' }
+        },
+        {
+            id: 2, photoUrl: 'https://resizer.mail.ru/p/7a1e86df-9686-5098-963e-27304c6f1133/AAACY37OntEVxnf9OOUuLkcQpyEwVtb3AZgK14eU9OI6IljiYtBizDmEp-vG8UfPC3h-OB130PE-ba1mk1rY6S-3Zek.jpg',
+            followed: true, fullName: 'Lena', status: 'Boss', location: { city: 'Moscow', country: 'Russia' }
+        },
+        {
+            id: 3, photoUrl: 'https://resizer.mail.ru/p/7a1e86df-9686-5098-963e-27304c6f1133/AAACY37OntEVxnf9OOUuLkcQpyEwVtb3AZgK14eU9OI6IljiYtBizDmEp-vG8UfPC3h-OB130PE-ba1mk1rY6S-3Zek.jpg',
+            followed: false, fullName: 'Stepa', status: 'Boss', location: { city: 'Kiev', country: 'Ukrain' }
+        },
+        {
+            id: 4, photoUrl: 'https://resizer.mail.ru/p/7a1e86df-9686-5098-963e-27304c6f1133/AAACY37OntEVxnf9OOUuLkcQpyEwVtb3AZgK14eU9OI6IljiYtBizDmEp-vG8UfPC3h-OB130PE-ba1mk1rY6S-3Zek.jpg',
+            followed: false, fullName: 'Vova', status: 'Boss', location: { city: 'Berlin', country: 'Germany' }
+        },
     ],
 };
 
@@ -29,7 +37,7 @@ const usersreducer = (state = initialState, action) => {
                 })
             }
         case SET_USERS:
-            return { ...state, users: [...state.users, ...action.users]}
+            return { ...state, users: [...state.users, ...action.users] }
         default:
             return state;
     }
